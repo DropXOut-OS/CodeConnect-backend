8a77ee6da97516aeb7ee0dfe6d3c44f042ef8a1b
wanishad <111111439+wanishad@users.noreply.github.com>
2024-03-07 02:33:50 -0800

    project setup completed

--- models/userModel.js
+++ models/userModel.js
@@ -1,19 +1,7 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
+// create a userModel
 }, { timestamps: true });
 
 const User = mongoose.model("User", userSchema);
---
+++ utils/cloudinary.js
@@ -0,0 +1,22 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs"       
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+const uploadCloudinary = async (localFilePath)=>{
+  try {
+    if(!localFilePath) return null
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto"
+    }
+      )
+      console.log("file is uploaded on cloudnary", response.url)
+      return response.url
+  } catch (error) {
+    fs.unlinkSync(localFilePath)
+    return null
+    
+  }
+}
