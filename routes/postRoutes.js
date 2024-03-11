64390c4635a36b2b903c6462469587dbcc570e86
wanishad <111111439+wanishad@users.noreply.github.com>
2024-03-10 19:06:44 -0700

    create post routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,8 +1,8 @@
-// import express from 'express';
-// import { upload } from '../middlewares/multerMiddlewares.js';
-// import { createPost } from '../controllers/postController.js';
+import express from 'express';
+import { upload } from '../middlewares/multerMiddlewares.js';
+import { createPost } from '../controllers/postController.js';
 
 
-// const Router = express.Router();
-// Router.post('/create-post', upload.single('image'), createPost)
-// export default Router
+const Router = express.Router();
+Router.post('/create-post', upload.single('image'), createPost)
+export default Router
