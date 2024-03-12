e7f0626fccee54d515b3ee4f797a35b7ab9cb7f3
wanishad <111111439+wanishad@users.noreply.github.com>
2024-03-12 01:19:14 -0700

    postcontroller createpost, deletepost, updatepost,

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import { upload } from '../middlewares/multerMiddlewares.js';
-import { createPost } from '../controllers/postController.js';
+import { createPost, deletePost, updatePost } from '../controllers/postController.js';
 import { isUserAuthenticated } from '../middlewares/authentiucate.js';
-
-
 const Router = express.Router();
+
+Router.use(isUserAuthenticated)
+
 Router.post('/create-post', upload.fields([
     {
         name: "image",
         maxCount: 1
     }
-]), isUserAuthenticated, createPost)
+]), createPost)
+
+Router.patch('/update-post/:id', updatePost)
+Router.delete('/delete-post/:id', deletePost)
 export default Router
--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -2,7 +2,6 @@ import {v2 as cloudinary} from 'cloudinary';
 import dotenv from "dotenv"
 dotenv.config()
 import fs from "fs"  
-console.log(process.env.CLOUDINARY_CLOUD_NAME)
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY, 
