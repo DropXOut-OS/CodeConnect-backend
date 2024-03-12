3666c9b207afdbf544bcf8b4a7318c57146916f9
wanishad <111111439+wanishad@users.noreply.github.com>
2024-03-11 17:43:11 -0700

    nothing changes

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,6 +1,6 @@
 import {v2 as cloudinary} from 'cloudinary';
 import fs from "fs"  
-
+console.log(process.env.CLOUDINARY_CLOUD_NAME)
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY, 
