c8e7c17c48e8749e230bc04be985d28d40716edd
Nitesh Prajapat <niteshp282000@gmail.com>
2024-03-13 15:34:22 +0530

    dependencies added

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 import express from "express"
 import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     Credential: true
 }))
-app.use(express.json({limit: "8kb"}))
-app.use(express.urlencoded({extended: true, limit: "8kb"}))
+
+app.use(cookieParser());
+app.use(express.json({ limit: "8kb" }))
+app.use(express.urlencoded({ extended: true, limit: "8kb" }))
 app.use(express.static("public"))
 
 
-export {app}
+export { app }
