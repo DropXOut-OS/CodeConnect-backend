bc22bff55dd0ac10b165d3707b7dc98fbd746505
Nitesh Prajapat <niteshp282000@gmail.com>
2024-03-14 17:37:05 +0530

    routes created for deleteComment and update comment

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -1,10 +1,14 @@
-import { createComment } from "../controllers/commentController.js";
-import { isUserAuthenticated } from "../middlewares/authentiucate.js";
-import  express from "express";
+import express from "express";
 const Router = express.Router()
+
+import { createComment, updateComment, deleteComment } from "../controllers/commentController.js";
+import { isUserAuthenticated } from "../middlewares/authentiucate.js";
+
 Router.use(isUserAuthenticated)
 
-Router.post('/create-comment/:postId', createComment)
+Router.post('/create-comment/:postId', createComment);
+Router.put('/update-comment/:postId', updateComment);
+Router.delete('/delete-comment/:postId', deleteComment);
 
 export default Router
 
